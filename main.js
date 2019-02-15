9e785a35dc7207012827ba9ab255985fbaa3b379
Borys Palacz <borka.92@gmail.com>
2019-02-15 23:12:28 +0100

    New concept, add fetch and style

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-  function createNode(element) {
-      return document.createElement(element);
-  }
-
-  function append(parent, el) {
-    return parent.appendChild(el);
-  }
-
-  const ul = document.getElementById('authors');
-  const url = 'https://api.openaq.org/v1/latest?country=PL&limit=200&parameter=pm25';
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    let authors = data.results;
-    return authors.map(function(author) {
-      let li = createNode('li'),
-          span = createNode('span');
-      span.innerHTML = `${author.measurements}`;
-      append(li, span);
-      append(ul, li);
+const url1 = "https://api.openaq.org/v1/latest?location=Poznan-Dabrowskiego&parameter=pm25";
+fetch(url1)
+    .then(resp => resp.json())
+    .then(resp => {
+        const pm25Value = resp.results[0].measurements[0].value;
+        const date = new Date (resp.results[0].measurements[0].lastUpdated);
+        const unit = resp.results[0].measurements[0].unit;
+        const fullResult = [Math.round(pm25Value),date,unit];
+        const hour = document.getElementById("hour");
+        hour.innerHTML = date.getUTCHours(date);
+        const unitDisp = document.getElementById("unit");
+        unitDisp.innerHTML = unit;
+        const pm25 = document.getElementById("pm25");
+        pm25.innerHTML = fullResult[0];
+        const norm = Math.round((pm25Value/25)*100);
+        const normPrint = document.getElementById("norm");
+        normPrint.innerHTML = norm;
+        const decision = document.getElementById("decision");
+        if (norm<=100){
+            decision.innerHTML = "Spokojnie, dzisiaj jesteś bezpieczny.";
+        } else{
+            decision.innerHTML = "Zostań w domu i nie otwieraj okien!";
+        }
+        
     })
-  })
-  .catch(function(error) {
-    console.log(JSON.stringify(error));
-  });   
+    .catch(error => {
+        console.warn('ups...', error);
+    })
