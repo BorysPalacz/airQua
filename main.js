45afdb9dcb6407e2aa5fa5e82340d78d2a5dc087
Borys Palacz <borka.92@gmail.com>
2019-02-27 22:52:15 +0100

    Add some content, still wait for button

--- main.js
+++ main.js
@@ -19,13 +19,22 @@ fetch(url1)
         const decision = document.getElementById("decision");
         if (norm<=100){
             decision.innerHTML = "Spokojnie, dzisiaj jesteś bezpieczny.";
-            document.body.style.backgroundColor = "red";
+            document.body.style.backgroundColor = "#63ff8f";
+            document.getElementById("myImg").src = "good.png"
              
         } else{
             decision.innerHTML = "Zostań w domu i nie otwieraj okien!";
-            document.body.style.backgroundColor = "blue";
+            document.body.style.backgroundColor = "#e07d7d";
+            document.getElementById("myImg").src = "good.png"
         }
-
+        function myFunction() {
+            let x = document.getElementsByClassName("is-size-5");
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          } 
         
     })
     .catch(error => {
